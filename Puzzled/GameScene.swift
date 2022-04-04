fa2b2a866f8db48518b7d5d2d47ddc7178734d4e
Awhite246 <awhite@bsd220.org>
2022-04-04 10:32:45 -0500

    created arrow

--- Puzzled/GameScene.swift
+++ Puzzled/GameScene.swift
@@ -7,7 +7,21 @@
 
 import SpriteKit
 import GameplayKit
-
 class GameScene: SKScene {
+    //variables and things
+    var arrow = SKSpriteNode()
     
+    //functions and things
+    override func didMove(to view: SKView) {
+        
+    }
+    func makeArrow() {
+        arrow.removeFromParent() //remove arrow if exists
+        arrow = SKSpriteNode(color: .red, size: CGSize(width: 200, height: 20))
+        arrow.position = CGPoint(x: frame.midX, y: frame.midY)
+        arrow.name = "arrow"
+        arrow.physicsBody = SKPhysicsBody(rectangleOf: arrow.size)
+        arrow.physicsBody?.isDynamic = false
+        addChild(arrow)
+    }
 }
