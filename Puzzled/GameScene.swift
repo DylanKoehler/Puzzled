e6db9eeebbed74af2a11b84fbe94e8c33c009c06
Dylan Koehler <Dylankoehler3212@gmail.com>
2022-04-04 10:55:54 -0500

    Tried Make Brick function but not finished yet just testing pull push with peter

--- Puzzled/GameScene.swift
+++ Puzzled/GameScene.swift
@@ -10,11 +10,15 @@ import GameplayKit
 class GameScene: SKScene {
     //variables and things
     var arrow = SKSpriteNode()
+    var bricks = [SKSpriteNode]()
+    var removedBricks = 0
+
     
     //functions and things
     override func didMove(to view: SKView) {
-        
+        makeBricks()
     }
+
     func makeArrow() {
         arrow.removeFromParent() //remove arrow if exists
         arrow = SKSpriteNode(color: .red, size: CGSize(width: 200, height: 20))
@@ -23,5 +27,25 @@ class GameScene: SKScene {
         arrow.physicsBody = SKPhysicsBody(rectangleOf: arrow.size)
         arrow.physicsBody?.isDynamic = false
         addChild(arrow)
+    }
+    func makeBricks() {
+        // first, remove any leftover bricks (from prior game)
+        for brick in bricks {
+            if brick.parent != nil {
+                brick.removeFromParent()
+            }
+        }
+        bricks.removeAll()  // clear the array
+        removedBricks = 0   // reset the counter
+        
+    }
+    // helper function used to make each brick
+    func makeBrick(x: Int, y: Int, color: UIColor) {
+        let brick = SKSpriteNode(color: color, size: CGSize(width: 50, height: 20))
+        brick.position = CGPoint(x: x, y: y)
+        brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
+        brick.physicsBody?.isDynamic = false
+        addChild(brick)
+        bricks.append(brick)
     }
 }
