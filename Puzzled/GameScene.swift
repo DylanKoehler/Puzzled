b4950372d6dfd73234e542f202716e3e7cd15dc8
Awhite246 <awhite@bsd220.org>
2022-05-05 10:23:35 -0500

    bug fixes

--- Puzzled/GameScene.swift
+++ Puzzled/GameScene.swift
@@ -9,13 +9,13 @@ import SpriteKit
 import GameplayKit
 class GameScene: SKScene, SKPhysicsContactDelegate {
     //variables and things
-    var arrow = SKSpriteNode()
+    var ball = SKShapeNode()
     var target = SKSpriteNode()
     var bouncyBricks = [SKSpriteNode]()
     var bricks = [SKSpriteNode]()
     var bow = SKSpriteNode()
     var currentBrick = SKSpriteNode() //when moving brick faster than touches moved can keep up, this variable fixes by storing last touched node
-    var arrowShot = false
+    var ballShot = false
     var winLabel = SKLabelNode()
     var nextLabel = SKLabelNode()
     var loseLabel = SKLabelNode()
@@ -34,23 +34,23 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         setLevel(level: currentLvl)
     }
     func didBegin(_ contact: SKPhysicsContact) {
-        if contact.bodyA.node?.name == "arrow" {
-            collisionBetween(arrow: contact.bodyA.node!, object: contact.bodyB.node!)
-        } else if contact.bodyB.node?.name == "arrow" {
-            collisionBetween(arrow: contact.bodyB.node!, object: contact.bodyA.node!)
+        if contact.bodyA.node?.name == "ball" {
+            collisionBetween(ball: contact.bodyA.node!, object: contact.bodyB.node!)
+        } else if contact.bodyB.node?.name == "ball" {
+            collisionBetween(ball: contact.bodyB.node!, object: contact.bodyA.node!)
         }
     }
-    func collisionBetween(arrow: SKNode, object: SKNode) {
-        //what happens when arrow hits target
+    func collisionBetween(ball: SKNode, object: SKNode) {
+        //what happens when ball hits target
         for brick in bricks {
             if object == brick {
-                arrow.physicsBody?.isDynamic = false
+                ball.physicsBody?.isDynamic = false
                 print("Lose")
                 resetLevel()
             }
         }
         if object.name == "target" {
-            arrow.physicsBody?.isDynamic = false
+            ball.physicsBody?.isDynamic = false
             nextLevel()
             print("Win")
         }
@@ -60,22 +60,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             }
         }
     }
-    func makeArrow(y: Int /* Changes the starting y position for 3 diff options */) {
-        arrow.removeFromParent() //remove arrow if exists
-        arrow = SKSpriteNode(imageNamed: "arrow")
-        arrow.position = CGPoint(x: frame.minX + 50, y: frame.midY + CGFloat((200 * y)))
-        arrow.name = "arrow"
+    func makeBall(y: Int /* Changes the starting y position for 3 diff options */) {
+        ball.removeFromParent() //remove ball if exists
+        ball = SKShapeNode(circleOfRadius: 10)
+        ball.position = CGPoint(x: frame.minX + 50, y: frame.midY + CGFloat((200 * y)))
+        ball.name = "ball"
         
-        arrow.physicsBody?.usesPreciseCollisionDetection = true
-        arrow.physicsBody = SKPhysicsBody(rectangleOf: arrow.size)
-        arrow.physicsBody!.contactTestBitMask = arrow.physicsBody!.collisionBitMask
-        arrow.physicsBody?.isDynamic = false
-        arrow.physicsBody?.friction = 0
-        arrow.physicsBody?.affectedByGravity = false
-        arrow.physicsBody?.restitution = 1
-        arrow.physicsBody?.linearDamping = 0
+        ball.physicsBody?.usesPreciseCollisionDetection = true
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+        ball.physicsBody!.contactTestBitMask = ball.physicsBody!.collisionBitMask
+        ball.physicsBody?.isDynamic = false
+        ball.physicsBody?.friction = 0
+        ball.physicsBody?.affectedByGravity = false
+        ball.physicsBody?.restitution = 1
+        ball.physicsBody?.linearDamping = 0
         
-        addChild(arrow)
+        addChild(ball)
     }
     func makeBow(y: Int) {
         bow.removeFromParent()
@@ -87,22 +87,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         bow.name = "bow"
         
         bow.physicsBody?.isDynamic = false
-        //makes it so arrow doesnt interact with the bow
+        //makes it so ball doesnt interact with the bow
         bow.physicsBody?.categoryBitMask = 0
-        //makes it so the bow is always under the arrow
+        //makes it so the bow is always under the ball
         bow.zPosition = -1
         
         addChild(bow)
     }
-    func shootArrow() {
-        arrow.physicsBody?.isDynamic = true
-        arrow.physicsBody?.applyImpulse(CGVector(dx: 15, dy: 0))
+    func shootBall() {
+        ball.physicsBody?.isDynamic = true
+        ball.physicsBody?.applyImpulse(CGVector(dx: 5, dy: 0))
     }
     func makeTarget(y : Int) {
         target.removeFromParent() //remove target if exists
         let targetPicture = SKTexture(imageNamed: "target")
         target = SKSpriteNode(texture: targetPicture, size: CGSize(width: 75, height: 75))
-        target.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 1, height: 75))
+        target.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 20, height: 20))
         target.position = CGPoint(x: frame.maxX - 50, y: frame.midY + CGFloat((200 * y)))
         target.zPosition = -1
         target.name = "target"
@@ -136,12 +136,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         bricks.append(brick)
     }
     
-    func makeBouncyBrick(x: Int, y: Int, color: UIColor) {
-        let bouncyBrick = SKSpriteNode(color: .blue, size: CGSize(width: 100, height: 20))
+    func makeBouncyBrick(x: Int, y: Int, canMove: Bool, type: Int) {
+        let brickPicture = SKTexture(imageNamed: "brick")
+        let bouncyBrick = SKSpriteNode(texture: brickPicture, color: canMove ? .systemPink : .systemPurple, size: CGSize(width: 100, height: 20))
+        bouncyBrick.colorBlendFactor = 0.9
         bouncyBrick.position = CGPoint(x: x, y: y)
         bouncyBrick.physicsBody = SKPhysicsBody(rectangleOf: bouncyBrick.size)
-        bouncyBrick.physicsBody?.isDynamic = true
+        bouncyBrick.physicsBody?.isDynamic = false
+        bouncyBrick.name = canMove ? "yesMove" : "noMove"
         bouncyBrick.physicsBody?.affectedByGravity = false
+        bouncyBrick.physicsBody?.allowsRotation = false
+        bouncyBrick.zRotation = 0.8 * CGFloat(type)
         addChild(bouncyBrick)
         bouncyBricks.append(bouncyBrick)
     }
@@ -188,9 +193,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         for touch in touches {
             let location = touch.location(in: self)
             for node in nodes(at: location) {
-                if !arrowShot {
+                if !ballShot {
                     for brick in bouncyBricks {
-                        if node == brick {
+                        if brick.name != "noMove" && node == brick {
                             currentBrick = brick
                             brick.physicsBody?.isDynamic = true
                             brick.position = location
@@ -204,8 +209,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                         }
                     }
                     if node.name == "bow" {
-                        shootArrow()
-                        arrowShot = true
+                        shootBall()
+                        ballShot = true
                         
                     }
                 }
@@ -226,7 +231,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         for touch in touches {
             let location = touch.location(in: self)
             //for node in nodes(at: location) {
-            if !arrowShot {
+            if !ballShot {
                 for brick in bouncyBricks {
                     if currentBrick == brick {
                         brick.position = location
@@ -245,14 +250,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     //when you take finger off it changes current so next time you press it wont jump to the old current
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         //fixes bricks that could be inside eachother.
-        for brick in bricks {
-            if (brick.name != "noMove"){
-                brick.physicsBody?.isDynamic = true
+        if currentBrick != target {
+            for brick in bricks {
+                if (brick.name != "noMove"){
+                    brick.physicsBody?.isDynamic = true
+                }
             }
-        }
-        for brick in bouncyBricks {
-            if (brick.name != "noMove"){
-                brick.physicsBody?.isDynamic = true
+            for brick in bouncyBricks {
+                if (brick.name != "noMove"){
+                    brick.physicsBody?.isDynamic = true
+                }
             }
         }
         currentBrick = target
@@ -284,24 +291,24 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     func setLevel(level : Int) {
         currentLvl = level
-        arrowShot = false
+        ballShot = false
         clearBricks()
         switch currentLvl {
         case 0: //for testing
-            makeArrow(y: -1)
+            makeBall(y: -1)
             makeTarget(y: -1)
             makeBow(y: -1)
-            makeBouncyBrick(x: 50, y: 50, color: .magenta)
+            makeBouncyBrick(x: 50, y: 50, canMove: true, type: 1)
             makeBrick(x: 100, y: 100, canMove: true)
             makeBrick(x: 0, y: 100, canMove: true)
             makeBrick(x: -100, y: 100, canMove: false)
         case 1: //level 1
             makeBow(y: 1)
-            makeArrow(y: 1)
+            makeBall(y: 1)
             makeTarget(y: -1)
-            makeBouncyBrick(x: 50, y: 50, color: .magenta)
-            makeBouncyBrick(x: 100, y: 100, color: .magenta)
-            makeBouncyBrick(x: 100, y: 50, color: .magenta)
+            makeBouncyBrick(x: 50, y: 50, canMove: true, type: 1)
+            makeBouncyBrick(x: 100, y: 100, canMove: true, type: 1)
+            makeBouncyBrick(x: 100, y: 50, canMove: true, type: -1)
             return
         default:
             return
